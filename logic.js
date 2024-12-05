900d6b2aaae63ab67153d7b82d6de7950378b141
avnish404 <avnishshukla0404@gmail.com>
2024-12-05 23:08:50 +0530

    Feature Addition
    Scoore updation, Array clean up and try again logic implementation

--- logic.js
+++ logic.js
@@ -1,8 +1,14 @@
 let screen = document.querySelector(".screen")
 let buttons = document.querySelectorAll(".btn")
+let foot=document.querySelector(".foot");
+let p= document.querySelector(".p")
+let p2= document.querySelector(".p2")
+
 let currentIndex = 0;
 let score=0;
 let quizData
+
+let nextBtn = document.querySelector(".next");
 
 // console.log(screen)
 
@@ -29,11 +35,12 @@ function fetchApi() {
 async function loadData() {
     try {
         let quizData = await fetchApi();
-        let nextBtn = document.querySelector(".next");
+        
+let currentIndex = 0;
        
         nextBtn.addEventListener('click', () => {
             if (currentIndex < quizData.results.length) {
-                let buttonsArr = []      // Creating an array of options
+                let buttonsArr = []   
                 screen.innerText = quizData.results[currentIndex].question     //Logic to push the current question text into the screen
                 buttonsArr.push(quizData.results[currentIndex].correct_answer)  // pushing options to the array of buttons i.e correct answer
 
@@ -48,6 +55,7 @@ async function loadData() {
                 /* Loop statement to assign elements of array of buttons into the buttons of options and check the correct answer*/
                 buttons.forEach((button, i)=>{
                     button.innerText = buttonsArr[i]
+                  
 
                     button.onclick=()=>
                     {      console.log("Button text:", button.innerText);
@@ -56,13 +64,28 @@ async function loadData() {
                         {
                             score++;
                             console.log("Correct! Score:", score);
+                          
+                            p.innerText=`Correct Answer now your score is ${score}`
+                         
+                           
                         }
                         else
                         {
                             console.log("Wrong Answer")
+                            p.innerText="Wrong Answer"
                         }
-                     
+
+                            //Disabling button after click
+                        buttons.forEach((btn)=>
+                            {
+                                btn.disabled=true;
+                            })
+                       
                     }
+
+                    button.disabled=false;  //Resetting clicking property of button after click
+
+                  
 
                    
                 })
@@ -70,6 +93,24 @@ async function loadData() {
 
                 currentIndex++;
                 console.log('Your Current Index is:',currentIndex);
+                if(currentIndex === quizData.results.length)
+                {   p.style.display='block'
+                    p.innerText='Game Over!'
+                    p2.style.display='block'
+                    p2.innerText=`Your total score is ${score}`
+                    screen.innerText=' Question will be posted here'
+
+                    nextBtn.disabled=true;
+                        //Disabling button after click
+                        buttons.forEach((btn,i)=>
+                            {
+                                btn.innerText=`Option ${i}`
+                                btn.disabled=true;
+                            })
+
+                    tryAgain();
+                }
+                
                 // checkAnswer();
             } 
             
@@ -89,32 +130,38 @@ async function loadData() {
 
 }
 
+function tryAgain()
+{
+    let a=document.createElement("button")
+    a.className='try'
+    a.innerText='Try again'
+    foot.appendChild(a)
+
+    currentIndex=0;
+   a.addEventListener('click',()=>
+{
+    loadData()
+    p.style.display='none'
+    p2.style.display='none'
+    a.style.display='none'
+    nextBtn.disabled=false;
+
+
+
+    
+})
+}
+
 loadData()
 
 
-// function checkAnswer()
-// {      
-//         buttons.forEach((button)=>
-//         {
-//             button.addEventListener('click',()=>
-//                 {
-//                     if(button.innerText === quizData.results[currentIndex-1].correct_answer)
-//                     {
-//                             score++;
-                           
-            
-//                     }
-//                     else{
-//                         console.log('Wrong answer')
-//                     }
-//                     console.log(score)
-                   
-//                 })
-//         })
-    
-// }
 
 
 
 // in line 41 why forEach loop was not working fine but map worked fine
+
+// after reloading data score was not getting displayed (correct and wrong answers)
+// randomising option was in effect after completion of quiz
+// container should get disappeared when game gets finished only try again button should be there
+// Encoding issue in qusetion and options 
 
